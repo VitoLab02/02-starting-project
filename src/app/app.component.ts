744e7bce847c469ce20cb05bff77ed7f99e98380
vito <vitolaba00@gmail.com>
2025-02-24 22:18:42 +0100

    Creating and using Reusable Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
-import { TrafficComponent } from "./dashboard/traffic/traffic.component";
-import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,9 @@ import { TicketsComponent } from "./dashboard/tickets/tickets.component";
     HeaderComponent,
     ServerStatusComponent,
     TrafficComponent,
-    TicketsComponent
-],
+    TicketsComponent,
+    DashboardItemComponent,
+  ],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
- 
-}
+export class AppComponent {}
